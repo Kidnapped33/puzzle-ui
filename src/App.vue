18daaefd7b327e26a2180447635432f1f774585c
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-10-27 22:13:24 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>hi</div>
+    <Ashine/>
+</template>
+
+<script>
+    import Ashine from "./components/Ashine.vue";
+
+    export default {
+        name: 'App',
+        components: {
+            Ashine,
+        }
+    }
+</script>

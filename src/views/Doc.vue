8492b8a353bd14d77b9de6a5f1b82813402d3c19
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-10-30 00:04:50 +0800

    初始化 aside HTML

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,14 +2,30 @@
 <div>
     <Topnav />
     <div class="content">
-        <aside>边栏</aside>
+        <aside>
+            <h2>组件列表</h2>
+            <ol>
+                <li>
+                    <routerlin to="/doc/switch">Switch 组件</routerlin>
+                </li>
+                <li>
+                    <routerlin to="/doc/button">Button 组件</routerlin>
+                </li>
+                <li>
+                    <routerlin to="/doc/dialog">Dialog 组件</routerlin>
+                </li>
+                <li>
+                    <routerlin to="/doc/tabs">Tabs 组件</routerlin>
+                </li>
+            </ol>
+        </aside>
         <main>主内容</main>
     </div>
 </div>
 </template>
 
 <script lang="ts">
-import Topnav from '../components/Topnav.vue'
+import Topnav from "../components/Topnav.vue";
 export default {
     components: {
         Topnav

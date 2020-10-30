4c2986e89f826ba28b858418458f96fc7fc27032
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-10-30 15:11:28 +0800

    switch css 1

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,5 +1,14 @@
 <template>
 <div>
-    <div>switch CONTENT</div>
+    <Switch />
 </div>
 </template>
+
+<script lang="ts">
+import Switch from "../lib/Switch.vue";
+export default {
+    components: {
+        Switch
+    }
+};
+</script>

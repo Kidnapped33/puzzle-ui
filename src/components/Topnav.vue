6573d3ed732534224d88263c06c225119911f441
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-10-30 11:16:32 +0800

    更新切换命名 让 Topnav 实现响应式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,49 +1,64 @@
 <template>
-  <div class="topnav">
+<div class="topnav">
     <div class="logo" @click="toggleAside">LOGO</div>
     <ul class="menu">
-      <li>菜单1</li>
-      <li>菜单2</li>
+        <li>菜单1</li>
+        <li>菜单2</li>
     </ul>
-  </div>
+</div>
 </template>
 
 <script lang="ts">
-import { inject, Ref } from "vue";
+import {
+    inject,
+    Ref
+} from "vue";
 export default {
-  setup() {
-    const asideVisible = inject<Ref<boolean>>("asideVisible");
-    const toggleAside = () => {
-      asideVisible.value = !asideVisible.value;
-    };
-    return {
-      toggleAside
-    };
-  }
+    setup() {
+        const asideVisible = inject < Ref < boolean >> ("asideVisible");
+        const toggleAside = () => {
+            asideVisible.value = !asideVisible.value;
+        };
+        return {
+            toggleAside
+        };
+    }
 };
 </script>
 
 <style lang="scss" scoped>
 .topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  position: relative;
-  z-index: 1;
-
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-
-  > .menu {
+    background: pink;
     display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
+    padding: 16px;
+    position: relative;
+    z-index: 10;
+    justify-content: center;
+    align-items: center;
 
-    > li {
-      margin: 0 1em;
+    >.logo {
+        max-width: 6em;
+        margin-right: auto;
     }
-  }
+
+    >.menu {
+        display: flex;
+        white-space: nowrap;
+        flex-wrap: nowrap;
+
+        >li {
+            margin: 0 1em;
+        }
+    }
+
+    @media (max-width: 500px) {
+        >.menu {
+            display: none;
+        }
+
+        >.logo {
+            margin: 0 auto;
+        }
+    }
 }
 </style>

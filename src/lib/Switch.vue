f89581cc3790f1c802fbb81333dd9357f7873dd2
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-10-30 15:53:15 +0800

    switch 滑动效果

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,33 +1,62 @@
 <template>
 <div>
-    <button><span></span></button>
+    <button @click="toggle" :class="{ checked: checked }"><span></span></button>
 </div>
 </template>
 
-<script>
-export default {};
+<script lang="ts">
+import {
+    ref
+} from "vue";
+export default {
+    setup() {
+        const checked = ref(false);
+        const toggle = () => {
+            checked.value = !checked.value;
+        };
+        return {
+            checked,
+            toggle
+        };
+    }
+};
 </script>
 
 <style lang="scss" scoped>
 $h: 22px;
-$h2: $h - 4px;
 
 button {
     height: $h;
     width: $h * 2;
     border: none;
-    background: orange;
+    background: gray;
     border-radius: $h/2;
     position: relative;
+    cursor: pointer;
 }
 
 span {
     position: absolute;
-    top: 2px;
-    left: 2px;
-    height: $h2;
-    width: $h2;
+    height: $h;
+    width: $h;
     background: white;
-    border-radius: $h2/2;
+    left: 0px;
+    top: 0px;
+    border-radius: $h/2;
+    // transition: all 1s linear;
+    transition: left 250ms;
+}
+
+button.checked {
+    background: rgb(36, 173, 243);
+}
+
+button.checked>span {
+    position: absolute;
+    left: $h;
+}
+
+button:focus {
+    outline: none;
 }
 </style>

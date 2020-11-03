5e244b27cc5847e31c74006ae438745c80d97a7f
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-03 22:02:37 +0800

    创建 toggle

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
-    Dialog
+    <div>
+      Dialog
+    </div>
+    <h1>示例1</h1>
+    <Button @click="toggle">切换</Button>
+    <Dialog :visible="x"></Dialog>
   </div>
-  <h1>示例1</h1>
-  <Dialog></Dialog>
 </template lang="ts">
 
 <script>
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import {
+    ref
+} from "vue";
+
 export default {
     components: {
-        Dialog
+        Dialog,
+        Button
+    },
+    setup() {
+        const x = ref(false);
+        const toggle = () => {
+            x.value = !x.value;
+        };
+        return {
+            x,
+            toggle
+        };
     }
 };
 </script>

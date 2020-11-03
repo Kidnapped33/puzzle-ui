4dc4325bf6cf614f622e488daf3d7646f8616989
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-03 15:12:49 +0800

    更新 CSS SIZE

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -8,6 +8,22 @@
         <Button theme="link">你好</Button>
         <Button theme="text">你好</Button>
     </div>
+    <h1>示例2</h1>
+    <div>
+        <Button size="big">大大大</Button>
+        <Button>正常</Button>
+        <Button size="small">小小小</Button>
+    </div>
+    <div>
+        <Button size="big" theme="link">大大大</Button>
+        <Button theme="link">正常</Button>
+        <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+        <Button size="big" theme="text">大大大</Button>
+        <Button theme="text">正常</Button>
+        <Button size="small" theme="text">小小小</Button>
+    </div>
 </div>
 </template>
 
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,38 @@
 <template>
-<button class="puzzle-button" :class="`puzzle-theme-${theme}`">
+<button class="puzzle-button" :class="classes">
     <slot />
 </button>
 </template>
 
 <script lang="ts">
+import {
+    computed
+} from "vue";
 export default {
     props: {
         theme: {
             type: String,
             default: "button"
+        },
+        size: {
+            type: String,
+            default: "normal"
         }
+    },
+    setup(props) {
+        const {
+            theme,
+            size
+        } = props;
+        const classes = computed(() => {
+            return {
+                [`puzzle-theme-${theme}`]: theme,
+                [`puzzle-size-${size}`]: size
+            };
+        });
+        return {
+            classes
+        };
     }
 };
 </script>
@@ -76,6 +98,18 @@ $radius: 4px;
         &:focus {
             background: darken(white, 5%);
         }
+    }
+
+    &.puzzle-size-big {
+        font-size: 24px;
+        height: 48px;
+        padding: 0 16px;
+    }
+
+    &.puzzle-size-small {
+        font-size: 12px;
+        height: 20px;
+        padding: 0 4px;
     }
 }
 </style>

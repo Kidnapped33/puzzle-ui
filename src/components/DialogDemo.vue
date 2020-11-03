c302b18eaac907e08c5569a82ef1e71cfe36ee52
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-03 20:43:16 +0800

    创建 Dialog 组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,5 +1,16 @@
 <template>
-<div>
+  <div>
     Dialog
-</div>
-</template>
+  </div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
+</template lang="ts">
+
+<script>
+import Dialog from "../lib/Dialog.vue";
+export default {
+    components: {
+        Dialog
+    }
+};
+</script>
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <div class="dialog-overlay"></div>
+    <div class="dialog-wrapper">
+        <header></header>
+        <main>
+            <p>第一行字</p>
+            <p>第二行字</p>
+        </main>
+        <footer>
+            <Button>OK</Button>
+            <Button>Cancel</Button>
+        </footer>
+    </div>
+</div>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+    components: {
+        Button
+    }
+};
+</script>

48dc41320ff3679f71653588f59e0e28e5d8c3b8
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-04 15:32:25 +0800

    完成 Tabs CSS

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,14 @@
 <template>
-    <div>
-        <div v-for="(t,index)in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="puzzle-tabs">
+        <div class="puzzle-tabs-nav">
+            <div class="puzzle-tabs-nav-item"
+                 v-for="(t,index) in titles" :key="index">{{t}}
+            </div>
+        </div>
+        <div class="puzzle-tabs-content">
+            <component class="puzzle-tabs-content-item"
+                       v-for="(c,index) in defaults" :is="c" :key="index"/>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -23,3 +30,33 @@
     };
 
 </script>
+<style lang="scss">
+    $green: rgb(126, 223, 62);
+    $color: #333;
+    $border-color: #d9d9d9;
+    .puzzle-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+
+                &:first-child {
+                    margin-left: 0;
+                }
+
+                &.selected {
+                    color: $green;
+                }
+            }
+        }
+
+        &-content {
+            padding: 8px 0;
+        }
+    }
+</style>

a7b466e3232d32dc65b666a535adeed153c006e5
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-04 15:16:15 +0800

    渲染tabs内容

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,3 +1,3 @@
 <template>
-    <div>tab</div>
+    <slot/>
 </template>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>tabs
-        <component :is="defaults[0]"/>
-        <component :is="defaults[1]"/>
+    <div>
+        <div v-for="(t,index)in titles" :key="index">{{t}}</div>
+        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
     </div>
 </template>
 <script lang="ts">
@@ -9,15 +9,17 @@
 
     export default {
         setup(props, context) {
+            //setup 的第二个参数得到类型
             const defaults = context.slots.default();
             defaults.forEach((tag) => {
-                    if (defaults[0].type !== Tab) {
-                        throw new Error('Tab 的子标签必须是 Tab');
-                    }
+                if (defaults[0].type !== Tab) {
+                    throw new Error('Tab 的子标签必须是 Tab');
                 }
-            );
-            return {defaults};
+            });
+            //获取 titles 导航1/2
+            const titles = defaults.map((tag) => {return tag.props.title;});
+            return {defaults, titles};
         }
-    }
+    };
 
 </script>

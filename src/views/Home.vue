19251ecde2aa3c8d619e4a977315eac16f334c14
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-05 12:56:44 +0800

    使用 iconfont 的 svg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-<div class="topnavAndBanner">
-    <Topnav />
-    <div class="banner">
-        <h1>时间 UI</h1>
-        <h2>一个厉害的 UI 框架</h2>
-        <p class="actions">
-            <a href="https://github.com/Kidnapped33" target="_blank"> GitHub</a>
-            <router-link to="/doc">开始</router-link>
-        </p>
+<div>
+    <div class="topnavAndBanner">
+        <Topnav />
+        <div class="banner">
+            <h1>时间 UI</h1>
+            <h2>一个厉害的 UI 框架</h2>
+            <p class="actions">
+                <a href="https://github.com/Kidnapped33" target="_blank"> GitHub</a>
+                <router-link to="/doc">开始</router-link>
+            </p>
+        </div>
+    </div>
+    <div class="features">
+        <svg class="icon">
+            <use xlink:href="#icon-vue"></use>
+        </svg>
+        <svg class="icon">
+            <use xlink:href="#icon-ts"></use>
+        </svg>
+        <svg class="icon">
+            <use xlink:href="#icon-light"></use>
+        </svg>
     </div>
 </div>
 </template>
@@ -22,13 +35,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$lightgreen: #02bcb0;
+$border-radius: 4px;
+$color: #007974;
+
 .topnavAndBanner {
     background: linear-gradient(145deg,
             rgba(227, 255, 253, 1) 0%,
             rgba(183, 233, 230, 1) 100%);
 }
 
+.features {
+    >svg {
+        width: 40px;
+        height: 40px;
+    }
+}
+
 .banner {
+    color: $color;
     padding: 100px 0;
     display: flex;
     justify-content: center;
@@ -40,13 +65,15 @@ export default {
 
         a {
             margin: 0 8px;
-            background: #fff;
+            background: $lightgreen;
+            color: white;
             display: inline-block;
-            $h: 28px;
-            height: $h;
-            line-height: $h;
-            border-radius: $h/2;
-            padding: 0 8px;
+            border-radius: $border-radius;
+            padding: 8px 24px;
+
+            &:hover {
+                text-decoration: none;
+            }
         }
     }
 }

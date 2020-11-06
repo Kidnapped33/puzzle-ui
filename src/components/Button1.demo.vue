f238bda3a39cff9135df276c2dc0fc8afcf17f2e
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-06 12:03:49 +0800

    抽离归纳Button

---
+++ src/components/Button1.demo.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+    <h1>常规使用</h1>
+    <div>
+        <Button theme="text">开始</Button>
+        <Button theme="link">开始</Button>
+        <Button>开始</Button>
+        <Button theme="shrink">开始</Button>
+        <Button theme="stretch">开始</Button>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+    components: {
+        Button
+    }
+};
+</script>
---
+++ src/components/Button5.demo.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+    <h1>支持显示加载中</h1>
+    <div>
+        <Button loading>加载中</Button>
+        <Button>加载完毕</Button>
+    </div>
+</div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+    components: {
+        Button
+    }
+};
+</script>

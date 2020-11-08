d0a1b7d53b42108bbac9b1a36262866c24b4834a
Ashine33 <8160042+ashine33@user.noreply.gitee.com>
2020-11-08 12:04:22 +0800

    创建 lib/index.ts

---
+++ src/lib/index.ts
@@ -0,0 +1,8 @@
+// import Switch from './Swich.vue'
+// export {Switch}
+export { default as Switch } form './Switch.vue';
+export { default as Button } form './Button.vue';
+export { default as Tabs } form './Tabs.vue';
+export { default as Tab } form './Tab.vue';
+export { default as Dialog } form './Dialog.vue';
+export { openDialog as openDialog } form './openDialog';
